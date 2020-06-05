805d5f73940ac7efc0268dfbfa7e122ab22cab5a
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-04 21:11:14 -0300

    creates FilterBar component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import "./App.css";
 
 import Topbar from "./components/Topbar";
+import FilterBar from "./components/FilterBar";
 
 function App() {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <FilterBar />
+    </>
+  );
 }
 
 export default App;
