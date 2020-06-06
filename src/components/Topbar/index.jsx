6e8c94c461d28b8d0e855878c052128973467903
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-06 19:20:10 -0300

    Creates Bag component

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiSearch, FiShoppingBag } from "react-icons/fi";
 
 import "./styles.scss";
 
+import Bag from "../Bag";
+
 const Topbar = () => {
+  const [showBag, setShowBag] = useState(false);
+
+  function handleBag() {
+    setShowBag(!showBag);
+  }
+
   return (
-    <header className="topbar">
-      <div className="topbar__content">
-        <h1>Fashionista</h1>
+    <>
+      <header className="topbar">
+        <div className="topbar__content">
+          <h1>Fashionista</h1>
 
-        <div className="topbar__buttons-container">
-          <button className="topbar__button">
-            <FiSearch size={24} color="#ffffff" />
-          </button>
+          <div className="topbar__buttons-container">
+            <button className="topbar__button">
+              <FiSearch size={24} color="#ffffff" />
+            </button>
 
-          <button className="topbar__button">
-            <FiShoppingBag size={24} color="#ffffff" />
-          </button>
+            <button className="topbar__button" onClick={handleBag}>
+              <FiShoppingBag size={24} color="#ffffff" />
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {showBag && <Bag handleBag={handleBag} />}
+    </>
   );
 };
 
