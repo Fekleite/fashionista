e04873c20cb2ba4721c21d60816cb7ecbc993176
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-08 21:17:16 -0300

    Add redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import reducer from "./reducers/reducer";
+import rootReducer from "./reducers";
 
-const store = createStore(reducer);
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["products"],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
