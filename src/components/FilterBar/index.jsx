242a157db8de03b616c1329212a9e7f3c9fd9890
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-11 15:40:05 -0300

    Update FilterBar component

--- src/components/FilterBar/index.jsx
+++ src/components/FilterBar/index.jsx
@@ -5,14 +5,6 @@ import "./styles.scss";
 const FilterBar = ({ length }) => {
   return (
     <div className="filterBar">
-      <div className="filterBar__content">
-        <p>Classificar por:</p>
-        <div className="filterBar__buttons-container">
-          <button className="filterBar__button">Menor preço</button>
-          <button className="filterBar__button">Maior preço</button>
-        </div>
-      </div>
-
       <div className="filterBar__quantity-products">
         <span>{length} items</span>
       </div>
